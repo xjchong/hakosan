23397b14f43f2562475ee909ef5d5f537774c037
Xavier Chong <xchong@xchong-mbp.local>
2022-03-12 18:49:54 -0500

    Add piece values

--- Piece.gd
+++ Piece.gd
@@ -22,8 +22,26 @@ func highlight():
 
 func unhighlight():
 	highlight_rect.visible = false
-
 	
 	
 func set_label():
 	label.text = type
+
+
+func get_value():
+	match type:
+		'grass': return 5
+		'bush': return 20
+		'tree': return 100
+		'bonfire': return 500
+		'camp': return 1500
+		'house': return 5000
+		'mansion': return 20000
+		'tower': return 100000
+		'castle': return 500000
+		'rock': return 0
+		'mine': return 1000
+		'gold': return 10000
+		'treasure': return 50000
+		_: return 0
+		
